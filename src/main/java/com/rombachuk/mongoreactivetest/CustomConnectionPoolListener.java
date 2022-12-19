7879e9c8c0fc0ae2f0e668aa09058f28c717f9ea
Andrew Rombach <andrew.rombach@uk.ibm.com>
2022-12-19 10:35:36 +0000

    Thread safe atomic increments to CustomConnectionPoolListener

--- src/main/java/com/rombachuk/mongoreactivetest/CustomConnectionPoolListener.java
+++ src/main/java/com/rombachuk/mongoreactivetest/CustomConnectionPoolListener.java
@@ -11,25 +11,29 @@ import com.mongodb.event.ConnectionPoolCreatedEvent;
 import com.mongodb.event.ConnectionPoolListener;
 import com.mongodb.event.ConnectionReadyEvent;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 
 public class CustomConnectionPoolListener implements ConnectionPoolListener {
 
+    // Use thread-safe atomicInteger to get correct counts
+    //
     private static Logger logger = LoggerFactory.getLogger(App.class.getName());
-    private int checkout_success = 0;
-    private int checkout_fail = 0;
-    private int checkout_count = 0;
-    private int connection_success = 0;
-    private int connection_count = 0;
-    private int connection_ready = 0;
-
+    private AtomicInteger checkout_success = new AtomicInteger( 0 );
+    private AtomicInteger checkout_fail = new AtomicInteger( 0 );
+    private AtomicInteger checkout_count = new AtomicInteger( 0 );
+    private AtomicInteger connection_success = new AtomicInteger( 0 );
+    private AtomicInteger connection_count = new AtomicInteger( 0 );
+    private AtomicInteger connection_ready = new AtomicInteger( 0 );
+    
     private String formatCounts() {
-       String connection_result =  "{connections->[current:"+Integer.toString(connection_ready) + "/success:"+Integer.toString(connection_success)+
-        "/attempts:"+Integer.toString(connection_count)+"]}";
-       String checkout_result =  "{checkouts->[current:"+Integer.toString(checkout_count) + "/success:"+Integer.toString(checkout_success)+
-        "/fail:"+Integer.toString(checkout_fail)+"]}";
+       String connection_result =  "{connections->[current:"+connection_ready.toString() + "/success:"+connection_success.toString()+
+        "/attempts:"+connection_count.toString()+"]}";
+       String checkout_result =  "{checkouts->[current:"+checkout_count.toString() + "/success:"+checkout_success.toString()+
+        "/fail:"+checkout_fail.toString()+"]}";
        String result = connection_result.concat(checkout_result);
        return result;
     }
@@ -46,42 +50,42 @@ public class CustomConnectionPoolListener implements ConnectionPoolListener {
 
     @Override
     public void connectionCheckedOut(final ConnectionCheckedOutEvent event) {
-        this.checkout_count = this.checkout_count + 1;
-        this.checkout_success = this.checkout_success + 1;
+        this.checkout_count.incrementAndGet();
+        this.checkout_success.incrementAndGet();
         logger.info("ConnectionPoolListener [checkedout]: "+formatCounts());
 
     }
 
     @Override
     public void connectionCheckOutFailed(ConnectionCheckOutFailedEvent event) {
-        this.checkout_fail = this.checkout_fail + 1;
+        this.checkout_fail.incrementAndGet();
         logger.info("ConnectionPoolListener [checkoutfail]: "+formatCounts());
         logger.info("ConnectionPoolListener [checkoutfail]: "+event);
     }
 
     @Override
     public void connectionCheckedIn(final ConnectionCheckedInEvent event) {     
-        this.checkout_count = this.checkout_count - 1;
+        this.checkout_count.decrementAndGet();
         logger.info("ConnectionPoolListener [checkedin]: "+formatCounts());
     }
 
     @Override
     public void connectionCreated(ConnectionCreatedEvent event) {
-        this.connection_count = this.connection_count + 1;
+        this.connection_count.incrementAndGet();
         logger.info("ConnectionPoolListener [conncreate]: "+formatCounts());
 
     }
 
     @Override
     public void connectionReady(ConnectionReadyEvent event) {
-        this.connection_ready = this.connection_ready + 1;
-        this.connection_success = this.connection_success + 1;
+        this.connection_ready.incrementAndGet();
+        this.connection_success.incrementAndGet();
         logger.info("ConnectionPoolListener [connready]: "+formatCounts());
     }
 
     @Override
     public void connectionClosed(ConnectionClosedEvent event) {
-        this.connection_ready = this.connection_ready - 1;
+        this.connection_ready.decrementAndGet();
         logger.info("ConnectionPoolListener [connclosed]: "+formatCounts());
     }
 
